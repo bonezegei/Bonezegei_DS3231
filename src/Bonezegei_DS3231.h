2bc8b77687117c6e2fc519d27d719bce0058c7c0
Bonezegei <93502321+bonezegei@users.noreply.github.com>
2024-01-26 20:11:36 +0800

    Update Bonezegei_DS3231.h

--- src/Bonezegei_DS3231.h
+++ src/Bonezegei_DS3231.h
@@ -9,12 +9,25 @@
 #include <Arduino.h>
 #include <Wire.h>
 
-class Bonezegei_DS3231{
+class Bonezegei_DS3231 {
 public:
   Bonezegei_DS3231();
   Bonezegei_DS3231(uint8_t addr);
 
+  char begin();
+  char getTime();
+
   uint8_t _addr;
+  uint8_t _data[13];
+
+  uint8_t _minutes;  //0 - 60 seconds
+  uint8_t _seconds;  //0 - 60 minutes
+  uint8_t _hour;
+
+  uint8_t _day;     // 1-7  day of week
+  uint8_t _date;    // 1-31 date 
+  uint8_t _month;   // 1- 12 month
+  uint8t_t _year;   // 0- 99 year
 };
 
 #endif
