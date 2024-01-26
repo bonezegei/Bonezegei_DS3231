c710d7d95f9afd85907d0bbcb59a0cec78986516
Bonezegei <93502321+bonezegei@users.noreply.github.com>
2024-01-27 06:43:04 +0800

    Update setTimeAndDate.ino

--- example/setTimeAndDate/setTimeAndDate.ino
+++ example/setTimeAndDate/setTimeAndDate.ino
@@ -1,1 +1,34 @@
+/*
+  Set Time And Date
+  Author: Bonezegei (Jofel Batutay)
+  Date: Jan 26 2024
+*/
 
+#include <Bonezegei_DS3231.h>
+
+Bonezegei_DS3231 rtc(0x68);
+
+void setup() {
+  Serial.begin(115200);
+  rtc.begin();
+}
+
+void loop() {
+
+  if (rtc.getTime()) {
+    Serial.printf("Time %02d:%02d:%02d ", rtc.getHour(), rtc.getMinute(), rtc.getSeconds());
+
+    if (rtc.getFormat() == 12) {  // returns 12 or 24 hour format
+
+      if (rtc.getAMPM()) {  //return 0 = AM  1 =PM
+        Serial.print("AM  ");
+      } else {
+        Serial.print("PM  ");
+      }
+    }
+
+    Serial.printf("Date %02d-%02d-%d \n", rtc.getMonth(), rtc.getDate(), rtc.getYear());
+  }
+  
+  delay(1000);
+}
