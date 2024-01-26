19b68ee62468299f8e9e13e9fc088405b8c725b2
Bonezegei <93502321+bonezegei@users.noreply.github.com>
2024-01-27 06:44:24 +0800

    Update Bonezegei_DS3231.h

--- src/Bonezegei_DS3231.h
+++ src/Bonezegei_DS3231.h
@@ -16,6 +16,7 @@ public:
 
   char begin();
   uint8_t convert(uint8_t data);
+  uint8_t convertBCD(int data);
   char getTime();
 
   uint8_t getSeconds();  //return seconds
@@ -28,10 +29,10 @@ public:
   uint8_t getAMPM();     //return 0=AM  1=PM
   uint8_t getFormat();   //return 12 or 24 hour format
 
-  void setFormat(uint8_t fmt); // set time format 12 or 24
-  void setAMPM(uint8_t ampm);  // set AM or PM   PM=1 AM=0
-  void setTime(char *t);       // set time Hour:Minute:Seconds 
-  void setDate(char *d);       // set Date Month:Date:Year
+  void setFormat(uint8_t fmt);  // set time format 12 or 24
+  void setAMPM(uint8_t ampm);   // set AM or PM   PM=1 AM=0
+  void setTime(const char *t);  // set time Hour:Minute:Seconds
+  void setDate(const char *d);  // set Date Month:Date:Year
 
 private:
   uint8_t _addr;
@@ -41,7 +42,7 @@ private:
   uint8_t _seconds;  // 0 - 60 minutes
   uint8_t _hour;     // 1-12 or 0 -23
   uint8_t _ampm;     // 0=AM  1=PM
-  uint8_t _hour12;   // 12 or 24  hour format  
+  uint8_t _hour12;   // 12 or 24  hour format
 
   uint8_t _day;    // 1-7  day of week
   uint8_t _date;   // 1-31 date
