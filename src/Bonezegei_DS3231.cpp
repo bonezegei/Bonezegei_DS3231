e06a3a5f3032a8f2ab38a4547ddc00992e7f987c
Bonezegei <93502321+bonezegei@users.noreply.github.com>
2024-01-26 20:11:59 +0800

    Update Bonezegei_DS3231.cpp

--- src/Bonezegei_DS3231.cpp
+++ src/Bonezegei_DS3231.cpp
@@ -6,11 +6,43 @@
 
 #include "Bonezegei_DS3231.h"
 
-Bonezegei_DS3231::Bonezegei_DS3231(){
-  
+Bonezegei_DS3231::Bonezegei_DS3231() {
 }
 
-Bonezegei_DS3231::Bonezegei_DS3231(uint8_t addr){
-
+Bonezegei_DS3231::Bonezegei_DS3231(uint8_t addr) {
+  _addr = addr;
 }
 
+char Bonezegei_DS3231::begin() {
+  if (Wire.begin()) {
+    return 1;
+  } else {
+    return 0;
+  }
+}
+
+char Bonezegei_DS3231::getTime() {
+  Wire.beginTransmission(_addr);
+  Wire.write(0x00);
+  Wire.endTransmission();
+
+  int a = 0;
+  int r = Wire.requestFrom(_addr, 13);
+  while (Wire.available()) {
+    _data[a] = Wire.read();
+    a++;
+  }
+
+  //time
+  _seconds = _data[0] & 0x7F; //0 - 60 seconds
+  _minutes = _data[1] & 0x7F; //0 - 60 minutes
+  _hour = _data[2] & 0x1F;
+
+  //date
+  _day = _data[3];          // 1-7  day of week
+  _date = _data[4;          // 1-31 date 
+  _month = _data[5] &0x3f;  // 1- 12 month
+  _year = _data[6];         // 0- 99 year
+
+  return r;
+}
