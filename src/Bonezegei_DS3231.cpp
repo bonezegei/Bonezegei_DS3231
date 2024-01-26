11613de3d8b8df7094af135cfa796a666808cba8
Bonezegei <93502321+bonezegei@users.noreply.github.com>
2024-01-26 20:44:42 +0800

    Update Bonezegei_DS3231.cpp

--- src/Bonezegei_DS3231.cpp
+++ src/Bonezegei_DS3231.cpp
@@ -21,28 +21,62 @@ char Bonezegei_DS3231::begin() {
   }
 }
 
+uint8_t Bonezegei_DS3231::convert(uint8_t data) {
+  uint8_t result = data & 0x0f;
+  result += ((data & 0xf0) >> 4) * 10;
+  return result;
+}
+
 char Bonezegei_DS3231::getTime() {
   Wire.beginTransmission(_addr);
   Wire.write(0x00);
   Wire.endTransmission();
 
   int a = 0;
-  int r = Wire.requestFrom(_addr, 13);
+  int r = (uint8_t)Wire.requestFrom((int)_addr, 13);
   while (Wire.available()) {
     _data[a] = Wire.read();
     a++;
   }
 
   //time
-  _seconds = _data[0] & 0x7F; //0 - 60 seconds
-  _minutes = _data[1] & 0x7F; //0 - 60 minutes
-  _hour = _data[2] & 0x1F;
+  _seconds = convert(_data[0]);  //0 - 60 seconds
+  _minutes = convert(_data[1]);  //0 - 60 minutes
+  _hour =  convert(_data[2] & 0x1F);
 
   //date
-  _day = _data[3];          // 1-7  day of week
-  _date = _data[4;          // 1-31 date 
-  _month = _data[5] &0x3f;  // 1- 12 month
-  _year = _data[6];         // 0- 99 year
+  _day = _data[3];           // 1-7  day of week
+  _date = _data[4];          // 1-31 date
+  _month = _data[5] & 0x3f;  // 1- 12 month
+  _year = _data[6];          // 0- 99 year
 
   return r;
+}
+
+uint8_t Bonezegei_DS3231::getSeconds() {
+  return _seconds;
+}
+
+uint8_t Bonezegei_DS3231::getMinute() {
+  return _minutes;
+}
+
+uint8_t Bonezegei_DS3231::getHour() {
+  return _hour;
+}
+
+uint8_t Bonezegei_DS3231::getDay() {
+  return _day;
+}
+
+uint8_t Bonezegei_DS3231::getDate() {
+  return _date;
+}
+
+uint8_t Bonezegei_DS3231::getMonth() {
+  return _month;
+}
+
+uint8_t Bonezegei_DS3231::getYear() {
+  return _year;
 }
