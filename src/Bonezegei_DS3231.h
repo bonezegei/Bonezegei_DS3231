9631525064ee67011c00b34f472ecbc5b6ed84b9
Bonezegei <93502321+bonezegei@users.noreply.github.com>
2024-01-26 17:51:03 +0800

    Update Bonezegei_DS3231.h

--- src/Bonezegei_DS3231.h
+++ src/Bonezegei_DS3231.h
@@ -1,6 +1,20 @@
+/*
+  This Library is written for DS3231 RTC
+  Author: Bonezegei (Jofel Batutay)
+  Date: Jan 26 2024
+*/
+
+#ifndef _BONEZEGEI_DS3231_H_
+#define _BONEZEGEI_DS3231_H_
+#include <Arduino.h>
+#include <Wire.h>
 
 class Bonezegei_DS3231{
 public:
   Bonezegei_DS3231();
   Bonezegei_DS3231(uint8_t addr);
+
+  uint8_t _addr;
 };
+
+#endif
