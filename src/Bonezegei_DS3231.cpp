25a379be5123aae5415e7983cee836b452381dcc
Bonezegei <93502321+bonezegei@users.noreply.github.com>
2024-01-26 17:51:26 +0800

    Update Bonezegei_DS3231.cpp

--- src/Bonezegei_DS3231.cpp
+++ src/Bonezegei_DS3231.cpp
@@ -1,1 +1,16 @@
+/*
+  This Library is written for DS3231 RTC
+  Author: Bonezegei (Jofel Batutay)
+  Date: Jan 26 2024
+*/
+
+#include "Bonezegei_DS3231.h"
+
+Bonezegei_DS3231::Bonezegei_DS3231(){
+  
+}
+
+Bonezegei_DS3231::Bonezegei_DS3231(uint8_t addr){
+
+}
 
