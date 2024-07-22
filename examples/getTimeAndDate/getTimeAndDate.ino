ea1b22e923d36691868e558f0b36139e77bab514
Bonezegei <93502321+bonezegei@users.noreply.github.com>
2024-07-22 15:25:04 +0800

    Update getTimeAndDate.ino
    AVR compatible example

--- examples/getTimeAndDate/getTimeAndDate.ino
+++ examples/getTimeAndDate/getTimeAndDate.ino
@@ -2,6 +2,7 @@
   Get Time And Date
   Author: Bonezegei (Jofel Batutay)
   Date: Jan 26 2024
+  Updated : Jul 22, 2024
 */
 
 #include <Bonezegei_DS3231.h>
@@ -14,20 +15,24 @@ void setup() {
 }
 
 void loop() {
-
+  char d[32];
   if (rtc.getTime()) {
-    Serial.printf("Time %02d:%02d:%02d ", rtc.getHour(), rtc.getMinute(), rtc.getSeconds());
+    //Serial.printf("Time %02d:%02d:%02d ", rtc.getHour(), rtc.getMinute(), rtc.getSeconds());
+    sprintf(d, "Time %02d:%02d:%02d ", rtc.getHour(), rtc.getMinute(), rtc.getSeconds());
+    Serial.print(d);
 
     if (rtc.getFormat() == 12) {  // returns 12 or 24 hour format
 
-      if (rtc.getAMPM()) {  //return 0 = AM  1 =PM
-        Serial.print("AM  ");
-      } else {
+      if (rtc.getAMPM()) {  //return 0 = AM  1 = PM
         Serial.print("PM  ");
+      } else {
+        Serial.print("AM  ");
       }
     }
 
-    Serial.printf("Date %02d-%02d-%d \n", rtc.getMonth(), rtc.getDate(), rtc.getYear());
+    //Serial.printf("Date %02d-%02d-%d \n", rtc.getMonth(), rtc.getDate(), rtc.getYear());
+    sprintf(d, "Date %02d-%02d-%d \n", rtc.getMonth(), rtc.getDate(), rtc.getYear());
+    Serial.print(d);
   }
   
   delay(1000);
